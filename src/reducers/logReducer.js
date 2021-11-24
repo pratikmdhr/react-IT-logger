d4cc471ff1c1879cf336519f6f6a44feca7e20fd
Pratik <pratikmndh@gmail.com>
2021-11-23 21:33:45 -0500

    Add functionality to edit existing log and update it on server

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -4,6 +4,9 @@ import {
 	LOGS_ERROR,
 	ADD_LOG,
 	DELETE_LOG,
+	UPDATE_LOG,
+	SET_CURRENT,
+	CLEAR_CURRENT,
 } from '../actions/types';
 
 const initialState = {
@@ -27,17 +30,39 @@ export default (state = initialState, action) => {
 				logs: action.payload,
 				loading: false,
 			};
+
 		case ADD_LOG:
 			return {
 				...state,
 				logs: [...state.logs, action.payload],
 				loading: false,
 			};
+
 		case DELETE_LOG:
 			return {
 				...state,
 				logs: state.logs.filter((log) => log.id !== action.payload),
 				loading: false,
+			};
+      
+		case UPDATE_LOG:
+			return {
+				...state,
+				logs: state.logs.map((log) =>
+					log.id === action.payload.id ? action.payload : log
+				),
+			};
+
+		case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload,
+			};
+
+		case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null,
 			};
 
 		case LOGS_ERROR:
