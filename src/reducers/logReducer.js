97db68059bea51cf79205e11ce8aa1ca9e13d212
Pratik <pratikmndh@gmail.com>
2021-11-24 22:03:51 -0500

    Add functionality to search logs on server

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -5,6 +5,7 @@ import {
 	ADD_LOG,
 	DELETE_LOG,
 	UPDATE_LOG,
+	SEARCH_LOGS,
 	SET_CURRENT,
 	CLEAR_CURRENT,
 } from '../actions/types';
@@ -44,13 +45,18 @@ export default (state = initialState, action) => {
 				logs: state.logs.filter((log) => log.id !== action.payload),
 				loading: false,
 			};
-      
+
 		case UPDATE_LOG:
 			return {
 				...state,
 				logs: state.logs.map((log) =>
 					log.id === action.payload.id ? action.payload : log
 				),
+			};
+		case SEARCH_LOGS:
+			return {
+				...state,
+				logs: action.payload,
 			};
 
 		case SET_CURRENT:
