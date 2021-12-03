d23619f8f171c4884da85b20939a5cce37f5fdba
Pratik <pratikmndh@gmail.com>
2021-12-02 19:16:38 -0500

    Add functionality to add techs to the server

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 
@@ -9,7 +12,11 @@ const AddTechModal = () => {
 		if (firstName === '' || lastName === '') {
 			M.toast({ html: 'Please enter the first and last name' });
 		} else {
-			console.log(firstName, lastName);
+			addTech({
+				firstName,
+				lastName,
+			});
+			M.toast({ html: `${firstName} ${lastName} was added as a tech` });
 			setFirstName('');
 			setLastName('');
 		}
@@ -33,7 +40,7 @@ const AddTechModal = () => {
 					</div>
 				</div>
 
-        <div className='row'>
+				<div className='row'>
 					<div className='input-field'>
 						<input
 							type='text'
@@ -46,7 +53,6 @@ const AddTechModal = () => {
 						</label>
 					</div>
 				</div>
-
 			</div>
 			<div className='modal-footer'>
 				<a
@@ -60,4 +66,8 @@ const AddTechModal = () => {
 	);
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+	addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
