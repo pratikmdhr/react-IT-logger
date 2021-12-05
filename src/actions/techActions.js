3eb484d0e2d3cd903b2f9847ef23904e7d1149a6
Pratik <pratikmndh@gmail.com>
2021-12-04 21:20:30 -0500

    Add functionality to delete technician

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -27,29 +27,50 @@ export const getTechs = () => async (dispatch) => {
 };
 
 // Add techs to server
-export const addTech = tech => async dispatch => {
-  try {
-    setLoading();
+export const addTech = (tech) => async (dispatch) => {
+	try {
+		setLoading();
 
-    const res = await fetch('/techs', {
-      method: 'POST',
-      body: JSON.stringify(tech),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await res.json();
+		const res = await fetch('/techs', {
+			method: 'POST',
+			body: JSON.stringify(tech),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const data = await res.json();
 
-    dispatch({
-      type: ADD_TECH,
-      payload: data
-    });
-  } catch (err) {
-    dispatch({
-      type: TECHS_ERROR,
-      payload: err.response.statusText
-    });
-  }
+		dispatch({
+			type: ADD_TECH,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText,
+		});
+	}
+};
+
+// Delete techs from server
+export const deleteTech = (id) => async (dispatch) => {
+	try {
+		setLoading();
+
+		await fetch(`/techs/${id}`, {
+			method: 'DELETE',
+		});
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id,
+		});
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText,
+		});
+	}
 };
 
 // Set loading to true
